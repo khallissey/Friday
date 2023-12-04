e010ca54c1c36c3392bf6c5fb5d47ed6f1c81587
khallissey <135153890+khallissey@users.noreply.github.com>
2023-12-04 17:31:44 -0500

    Update Friday_mindM2.py

--- Friday_mindM2.py
+++ Friday_mindM2.py
@@ -14,7 +14,7 @@ bot = ChatBot(
         {
             'import_path': 'chatterbot.logic.BestMatch',
             'default_response': 'I am sorry, but I do not understand.',
-            'maximum_similarity_threshold': 0.70
+            'maximum_similarity_threshold': 0.50
         }
     ])
 trainer = ChatterBotCorpusTrainer(bot)
